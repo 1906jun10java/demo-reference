9e270120ec3b9ded39532666edc85e612fd84c44
Emily Higgins <emilyrchiggins@gmail.com>
2019-06-13 12:30:44 -0400

    fun with arrays

--- WeekA/Hello.java
+++ WeekA/Hello.java
@@ -1,7 +1,39 @@
+import java.util.Arrays; // just saves you typing! 
+
 public class Hello {
 
 	public static void main(String[] args) {
-		travel();
+		funWithArrays(new Car(1997, "Mazda", "Sundance"), new Boat("blue", "Tracker"), new Boat("red", "Champion"));
+	}
+
+
+	// overloaded funWithArrays
+	public static void funWithArrays(Vehicle v1, Vehicle v2, Vehicle v3) {
+		// put Vehicles in array and print it out
+		Vehicle[] vehicles = {v1, v2, v3};
+		System.out.println(Arrays.toString(vehicles));
+	}
+	
+
+	public static void funWithArrays() {
+		// how to create an array
+		String[] arr1 = {"this", "is", "an", "array"};
+		String[] arr2 = new String[3]; // indices 0-2
+		arr2[0] = "also";
+		arr2[1] = "an";
+		arr2[2] = "array";
+		// iterate through an array
+		// regular for loop
+		for (int i=0; i<arr1.length; i++) {
+			System.out.println(arr1[i]);
+		}	
+		// enhanced (augmented) for loop to iterate through Array or Iterable
+		for (String s : arr2) {
+			System.out.println(s);
+		}
+		// for printing arrays...
+		System.out.println("toString from Array: "+arr2.toString());
+		System.out.println("toString from Arrays utility class: "+Arrays.toString(arr2));
 	}
 	
 	public static void init() {
