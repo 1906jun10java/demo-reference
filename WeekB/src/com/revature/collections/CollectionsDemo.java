71c7a9d150dbb13e05c9869eb4d80acfb3beeb4f
Emily Higgins <emilyrchiggins@gmail.com>
2019-06-18 11:58:35 -0400

    list, set, queue

--- WeekB/src/com/revature/collections/CollectionsDemo.java
+++ WeekB/src/com/revature/collections/CollectionsDemo.java
@@ -1,62 +1,175 @@
 package com.revature.collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Vector;
 
-import com.revature.model.*; // importing all classes/interfaces in com.revature.model package
+import com.revature.model.User;
 
 public class CollectionsDemo {
-	
+
 	public static void main(String[] args) {
-		
-		funWithLists();
-		
+
+		funWithQueues();
+
 	}
 	
-	public static void funWithLists() {
+	public static void funWithQueues() {
 		
 		/*
-		 * List:
+		 * Queue:
 		 * 
-		 * -Lists are ordered Collections
-		 * -Duplicate elements are allowed
+		 * -typically for holding elements while waiting to process them
+		 * -provides methods for adding, removing, and, inspecting elements
+		 * -two versions for each of these behaviors: one which throws an exception if the operation fails,
+		 * and one which does not. 
+		 * -FIFO (first-in, first-out, except for PriorityQueue, which uses a Comparator or elements' natural ordering
+		 * -duplicates are allowed
 		 * 
-		 * Some additional List features (beyond methods defined in Collection):
-		 * -Positional access: access/manipulate elements based on position in List
-		 * -Search: search for an element and retrieve its numerical position
-		 * -Iteration: listIterator() expands on capabilities of Iterator
-		 * -Range-view: access/manipulate subsets of the List
-		 * 
-		 * https://docs.oracle.com/javase/8/docs/api/java/util/List.html
-		 *
+		 *  https://docs.oracle.com/javase/8/docs/api/java/util/Queue.html
 		 */
 		
-		List<User> users = new ArrayList<>(); 
-		// generally want to use the supertype of whatever Collection 
-		// User is the type of each element in the List, replaces the generic type parameter in List<E> specification
+		Queue<User> userQueue = new LinkedList<>();
 		
 		User u1 = new User("Gandalf", "Grey", "mithrandir", "you_shall_not");
 		User u2 = new User("Paul", "Hewson", "bono", "u2RuleZ");
 		User u3 = new User("David", "Tennant", "doc10", "tardis");
+
+		userQueue.add(u1);
+		userQueue.add(u2);
+		userQueue.add(u3);
+		userQueue.add(u1);
 		
-		users.add(u1);
-		users.add(u2);
-		users.add(u3);
+		while (userQueue.size() != 0) {
+			System.out.println("Queue size: "+userQueue.size());
+			System.out.println("Processing: "+ userQueue.poll()); // removes and returns head of queue
+		}
 		
-		// we have a addAll() method, but it takes a Collection
+	}
+	
+	public static void funWithSets() {
 		
-		// duplicates are allowed, so the following should work:
-		users.add(u1);
+		/*
+		 * Set:
+		 * 
+		 * -Sets are not ordered
+		 * -Duplicate elements are not allowed
+		 * -Two sets are equal if they have the same elements (enforced by equals() and hashcode() implementations)
+		 * 
+		 * https://docs.oracle.com/javase/8/docs/api/java/util/Set.html
+		 */
 		
-		for (User user : users) {
+		Set<User> userSet = new HashSet<>();
+		User u1 = new User("Gandalf", "Grey", "mithrandir", "you_shall_not");
+		User u2 = new User("Paul", "Hewson", "bono", "u2RuleZ");
+		User u3 = new User("David", "Tennant", "doc10", "tardis");
+
+		userSet.add(u1);
+		userSet.add(u2);
+		userSet.add(u3);
+		
+		// no duplicates... addition of duplicate elements will be ignored. 
+		userSet.add(u1); // same object (according to ==) as an existing element
+		
+		User u4 = new User("David", "Tennant", "doc10", "tardis");
+		userSet.add(u4); // different object (according to ==) with the same fields as an existing element
+		
+		// both are ignored, it's the .equals() comparison that's being used (equality, not identity)
+		
+		for (User user : userSet) {
 			System.out.println(user);
 		}
 		
-		
-		
+		// notice that insertion order is ignored! 
 		
 	}
 
+	public static void funWithLists() {
+
+		/*
+		 * List:
+		 * 
+		 * -Lists are ordered Collections -Duplicate elements are allowed
+		 * 
+		 * Some additional List features (beyond methods defined in Collection):
+		 * -Positional access: access/manipulate elements based on position in List
+		 * -Search: search for an element and retrieve its numerical position
+		 * -Iteration: listIterator() expands on capabilities of Iterator -Range-view:
+		 * access/manipulate subsets of the List
+		 * 
+		 * https://docs.oracle.com/javase/8/docs/api/java/util/List.html
+		 *
+		 */
+
+		List<User> users = new ArrayList<>();
+		// generally want to use the supertype of whatever Collection
+		// User is the type of each element in the List, replaces the generic type
+		// parameter in List<E> specification
+
+		User u1 = new User("Gandalf", "Grey", "mithrandir", "you_shall_not");
+		User u2 = new User("Paul", "Hewson", "bono", "u2RuleZ");
+		User u3 = new User("David", "Tennant", "doc10", "tardis");
+
+		users.add(u1);
+		users.add(u2);
+		users.add(u3);
+
+		// we have a addAll() method, but it takes a Collection
+
+		// duplicates are allowed, so the following should work:
+		users.add(u1);
+
+		for (User user : users) {
+			System.out.println(user);
+		}
+
+		System.out.println("Size of list: " + users.size());
+
+		System.out.println("---------------------------------------------------------");
+
+		// List of Integers
+		List<Integer> integerList = new Vector<>();
+		// must use wrapper class Integer
+		// wrapper class - datatype which wraps around a primitive value
+		// every primitive datatype has a corresponding wrapper class
+
+		integerList.add(10); // implicitly converting from int (primitive) to Integer (Object). This is
+								// called autoboxing
+		integerList.add(-255);
+		integerList.add(new Integer(9)); // this is NOT autoboxing!
+		integerList.add(75);
+		integerList.add(-12);
+
+		System.out.println("integer list, in insertion order");
+
+		for (Integer integer : integerList) {
+			System.out.println(integer);
+		}
+		
+
+		/*
+		 * Collections utility class has static methods for operating on Collection
+		 * objects.
+		 * 
+		 * sort() method:
+		 * must be used with a Collection of Comparable elements
+		 * (or be provided a Comparator for that datatype)
+		 */
+		Collections.sort(integerList);
+
+		System.out.println("integer list, sorted");
+
+		for (Integer integer : integerList) {
+			System.out.println(integer);
+		}
+		
+		System.out.println("---------------------------------------------------------");
+
+	}
+
 }
-
-
