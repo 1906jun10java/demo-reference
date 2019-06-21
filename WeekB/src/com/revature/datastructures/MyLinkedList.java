50c52c196492800d6bd9ee7c55f172e1593982f8
Emily Higgins <emilyrchiggins@gmail.com>
2019-06-21 10:49:23 -0400

    removing duplicates

--- WeekB/src/com/revature/datastructures/MyLinkedList.java
+++ WeekB/src/com/revature/datastructures/MyLinkedList.java
@@ -6,18 +6,19 @@ public class MyLinkedList<T> {
 	}
 
 	// root node of the list, the 'head'
-	private Node<T> head;
+	private Node<T> head; // instance variable, which gets initialized to the default value for its
+							// datatype (here: null)
 
 	// end node of the list, the 'tail'
 	private Node<T> tail;
 
 	// insert a node at the end of the list
 	public void insert(T data) {
-		
+
 		// create a new Node for the data
 		Node<T> newNode = new Node<>(null, data);
-		
-		// if the head is null, this new node will become the head 
+
+		// if the head is null, this new node will become the head
 		if (this.head == null) {
 			System.out.println("List is empty, adding first element");
 			this.head = newNode;
@@ -51,6 +52,43 @@ public class MyLinkedList<T> {
 
 	// remove first node whose data matches provided key
 	public boolean removeByKey(T key) {
+
+		if (key == null) {
+			return false;
+		}
+
+		// create a reference to current node
+		Node<T> currentNode = this.head;
+
+		// create a reference to the next node (after the current)
+		Node<T> nextNode = currentNode.getNextNode();
+
+		// if first node matches the key, move head to next node
+		if (currentNode != null && currentNode.getData() != null && currentNode.getData().equals(key)) {
+			this.head = nextNode;
+			return true;
+		}
+
+		// iterate through list while currentNode != null
+		while (currentNode != null) {
+
+			// if nextNode is not null and matches the key
+			// point currentNode to the node after nextNode and return true
+			if (nextNode != null && nextNode.getData().equals(key)) {
+				currentNode.setNextNode(nextNode.getNextNode());
+				return true;
+			}
+
+			// advance currentNode to the next node in the list
+			currentNode = currentNode.getNextNode();
+
+			// if nextNode is not null, advance next node to the one after it
+			if (nextNode != null) {
+				nextNode = nextNode.getNextNode();
+			}
+
+		}
+
 		return false;
 	}
 
@@ -63,4 +101,52 @@ public class MyLinkedList<T> {
 		}
 	}
 
+	// remove all duplicate from list
+	public void removeDuplicates() {
+		Node<T> currentNode = this.head;
+		Node<T> tempNode;
+		T currentValue = null;
+
+		// System.out.println(tempNode); does not compile because variables in method
+		// and block/local scope do NOT receive default values the way the instance and
+		// static variables do.
+
+		// do we have a list in the first place?
+		if (currentNode != null) {
+			currentValue = currentNode.getData();
+			System.out.println("at beginning of list, initial value = " + currentValue);
+
+			do {
+				System.out.println("beginning outer loop iteration");
+				tempNode = currentNode; // have "runner" start in the same spot as the current node
+				// don't need to re-check earlier nodes in the list
+
+				while (tempNode != null && tempNode.getNextNode() != null) {
+					// check for matches
+					T nextValue = tempNode.getNextNode().getData();
+					System.out.println("comparing values ("+currentValue+" , "+nextValue+")");
+					if (currentValue.equals(nextValue)) {
+						System.out.println("removing first instance of the value: "+currentValue);
+						this.removeByKey(currentValue);
+					}
+					System.out.println("moving tempNode to next node");
+					tempNode = tempNode.getNextNode();
+				}
+
+				System.out.println("moving to currentNode to next node");
+				if (currentNode.getNextNode() != null) {
+					System.out.println("no more duplicates of " + currentValue + " left");
+					System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+					this.printList();
+					System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+					currentValue = currentNode.getNextNode().getData();
+					currentNode = currentNode.getNextNode();
+				}
+
+			} while (currentValue != null && currentNode.getNextNode() != null);
+			// exit iteration if current node's value is null OR there is no next node
+			// note: a refinement would be to scrub out or skip all nodes with null values
+			// instead of quitting as soon as we find one
+		}
+	}
 }
--- WeekB/src/com/revature/datastructures/MyLinkedListDemo.java
+++ WeekB/src/com/revature/datastructures/MyLinkedListDemo.java
@@ -5,41 +5,60 @@ import com.revature.model.User;
 public class MyLinkedListDemo {
 
 	public static void main(String[] args) {
-	
+
 		User u1 = new User("Gandalf", "Grey", "mithrandir", "you_shall_not");
 		User u2 = new User("Paul", "Hewson", "bono", "u2RuleZ");
 		User u3 = new User("David", "Tennant", "doc10", "tardis");
-		
+		User u4 = new User("Paul", "Hewson", "bono", "u2RuleZ");
+		User u5 = new User("Paul", "Hewson", "bono", "u2RuleZ");
+		User u6 = new User("David", "Tennant", "doc10", "tardis");
+
 		MyLinkedList<User> userList = new MyLinkedList<>();
-		
+
 		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		System.out.println("List before insertions");
 		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		userList.printList();
-		
+
 		userList.insert(u1);
 		userList.insert(u2);
 		userList.insert(u3);
-		
+		userList.insert(u4);
+		userList.insert(u5);
+		userList.insert(u6);
+
 		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		System.out.println("List after insertions");
 		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		userList.printList();
+
+		/*
+		 * System.out.println(
+		 * "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		 * System.out.println("Test peek()"); System.out.println(
+		 * "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		 * System.out.println("Head: "+userList.peek()); userList.printList();
+		 * 
+		 * System.out.println(
+		 * "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		 * System.out.println("Test poll()"); System.out.println(
+		 * "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		 * System.out.println("Head: "+userList.poll()); userList.printList();
+		 * 
+		 */
 		
 		
+		/*userList.removeByKey(u4);
+		
 		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-		System.out.println("Test peek()");
+		System.out.println("List after removeByKey");
 		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-		System.out.println("Head: "+userList.peek());
+		*/
+		
+		userList.removeDuplicates();
 		userList.printList();
 		
-		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-		System.out.println("Test poll()");
-		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-		System.out.println("Head: "+userList.poll());
-		userList.printList();
-		
-		
+
 	}
 
 }
