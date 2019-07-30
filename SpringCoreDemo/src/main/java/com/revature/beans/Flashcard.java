0a38e08ee407a16290f4868effd0792391cdd85a
erchiggins <emilyrchiggins@gmail.com>
2019-07-30 15:15:24 -0400

    adding some beans

---
+++ SpringCoreDemo/src/main/java/com/revature/beans/Flashcard.java
@@ -0,0 +1,54 @@
+package com.revature.beans;
+
+public class Flashcard {
+
+	public Flashcard(int id, String question, String answer, FlashcardTopic topic) {
+		super();
+		this.id = id;
+		this.question = question;
+		this.answer = answer;
+		this.topic = topic;
+	}
+
+	public Flashcard() {
+		super();
+	}
+
+	protected int id;
+	protected String question;
+	protected String answer;
+	protected FlashcardTopic topic;
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public FlashcardTopic getTopic() {
+		return topic;
+	}
+
+	public void setTopic(FlashcardTopic topic) {
+		this.topic = topic;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
