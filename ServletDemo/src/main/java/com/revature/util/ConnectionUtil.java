35dcd48fe94c753668f91806ad1eb3519872e8a2
erchiggins <emilyrchiggins@gmail.com>
2019-07-31 09:40:54 -0400

    adding properties, shh

--- ServletDemo/src/main/java/com/revature/util/ConnectionUtil.java
+++ ServletDemo/src/main/java/com/revature/util/ConnectionUtil.java
@@ -9,16 +9,17 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-	
+
 	public static Connection getConnectionFromFile(String filename) throws SQLException {
 		Connection c = null;
 		try {
-		Properties prop = new Properties();
-		InputStream in = ConnectionUtil.class.getClassLoader().getResourceAsStream(filename);
-		prop.load(in);
+			Properties prop = new Properties();
+			InputStream in = ConnectionUtil.class.getClassLoader().getResourceAsStream(filename);
+			prop.load(in);
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			c = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"), prop.getProperty("pass"));
-		} catch(ClassNotFoundException ex) {
+			c = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
+					prop.getProperty("pass"));
+		} catch (ClassNotFoundException ex) {
 			System.out.println("unable to load driver class!");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
