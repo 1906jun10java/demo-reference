59b4b511766b864059fc562a228a87e2e2f05234
erchiggins <emilyrchiggins@gmail.com>
2019-08-08 12:13:18 -0400

    adding discovery

--- boot-demo/src/main/java/com/revature/BootDemoApplication.java
+++ boot-demo/src/main/java/com/revature/BootDemoApplication.java
@@ -2,7 +2,9 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient // another version, framework-independent, which is EnableDiscoveryClient
 @SpringBootApplication
 public class BootDemoApplication {
 	
